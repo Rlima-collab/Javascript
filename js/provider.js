4c6358147b9f1734264cc1a8558e91f72cecea9c
Lima Romain <o22301253@or-iut-i003-f27.iut45.univ-orleans.fr>
2025-03-19 15:33:34 +0100

    liste des personnages

---
+++ js/provider.js
@@ -0,0 +1,33 @@
+import { ENDPOINT } from './config.js';
+
+export class Personnage {
+  constructor(id, nom, role, equipements, description, image, note) {
+    this.id = id;
+    this.nom = nom;
+    this.role = role;
+    this.equipements = equipements;
+    this.description = description;
+    this.image = image;
+    this.note = note;
+  }
+
+static async fetchAll() {
+    const response = await fetch(`${ENDPOINT}/personnages`);
+    const data = await response.json();
+    return data.map(item => new Personnage(item.id, item.nom, item.role, item.equipements, item.description, item.image, item.note));
+}
+
+  static async fetchById(id) {
+    const response = await fetch(`${ENDPOINT}/personnages/${id}`);
+    const data = await response.json();
+    return new Personnage(data.id, data.nom, data.role, data.equipements, data.description, data.image, data.note);
+  }
+
+  static async updateNote(id, note) {
+    await fetch(`${ENDPOINT}/personnages/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ note })
+    });
+  }
+}
