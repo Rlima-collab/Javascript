01286782c6cc5ea107a59c2c1d2ea4e9cff278c7
Lima Romain <o22301253@or-iut-i003-f26.iut45.univ-orleans.fr>
2025-03-26 16:47:56 +0100

    apparition nom des équipement, correction clique personnage

--- js/views/liste.js
+++ js/views/liste.js
@@ -3,39 +3,43 @@ import { Personnage } from '../provider.js';
 export function renderListe(content, page = 1) {
   
   Personnage.fetchAll(page).then(personnages => {
-    content.innerHTML = `
-      <h2>Liste des Personnages</h2>
-      <input type="text" id="search" placeholder="Rechercher un personnage">
-      <div class="card-list" id="liste">
-        ${personnages.map(p => `
-          <div class="card">
-            <img loading="lazy" src="${p.image}" alt="${p.nom}">
-            <a href="#detail/${p.id}">${p.nom}</a>
-            <p>Rôle: ${p.role}</p>
-          </div>
-        `).join('')}
+  content.innerHTML = `
+    <h2>Liste des Personnages</h2>
+    <input type="text" id="search" placeholder="Rechercher un personnage">
+    <div class="card-list" id="liste">
+    ${personnages.map(p => `
+      <a href="#detail/${p.id}">
+      <div class="card">
+      <img loading="lazy" src="${p.image}" alt="${p.nom}">
+      <p id="nom"> ${p.nom}</p>
+      <p>Rôle: ${p.role}</p>
       </div>
-      <div class="pagination">
-        <button id="prev">Précédent</button>
-        <button id="next">Suivant</button>
-      </div>
-    `;
-    document.getElementById('prev').addEventListener('click', () => {
-      if (page > 1) renderListe(content, page - 1);
-    });
-    document.getElementById('next').addEventListener('click', () => {
-      renderListe(content, page + 1);
-    });
-    document.getElementById('search').addEventListener('input', (e) => {
-      const query = e.target.value.toLowerCase();
-      const filtered = personnages.filter(p => p.nom.toLowerCase().includes(query));
-      document.getElementById('liste').innerHTML = filtered.map(p => `
-        <div class="card">
-          <img loading="lazy" src="${p.image}" alt="${p.nom}">
-          <a href="#detail/${p.id}">${p.nom}</a>
-          <p>Rôle: ${p.role}</p>
-        </div>
-      `).join('');
-    });
+      </a>
+    `).join('')}
+    </div>
+    <div class="pagination">
+    <button id="prev">Précédent</button>
+    <button id="next">Suivant</button>
+    </div>
+  `;
+  document.getElementById('prev').addEventListener('click', () => {
+    if (page > 1) renderListe(content, page - 1);
+  });
+  document.getElementById('next').addEventListener('click', () => {
+    renderListe(content, page + 1);
+  });
+  document.getElementById('search').addEventListener('input', (e) => {
+    const query = e.target.value.toLowerCase();
+    const filtered = personnages.filter(p => p.nom.toLowerCase().includes(query));
+    document.getElementById('liste').innerHTML = filtered.map(p => `
+    <a href="#detail/${p.id}">
+    <div class="card">
+      <img loading="lazy" src="${p.image}" alt="${p.nom}">
+      <p id="nom"> ${p.nom}</p>
+      <p>Rôle: ${p.role}</p>
+    </div>
+    </a>
+    `).join('');
+  });
   });
 }
